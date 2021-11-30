098a6bc4f6ea45ff53eb88d845166c1b25d83341
Caio Zanqueta <caiozan2001@hotmail.com>
2021-11-30 03:12:34 -0300

    Adding README

--- src/index.js
+++ src/index.js
@@ -2,8 +2,4 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hello World Ignite - Fundamentos NodeJS!'})
-})
-
 app.listen(3333)
